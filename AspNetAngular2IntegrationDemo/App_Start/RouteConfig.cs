cb6192867dd9231d3b0f278ef607dcb5889fbf49
Fabian Raetz <fabian.raetz@gmail.com>
2016-11-14 21:40:49 +0100

    initial commit

--- AspNetAngular2IntegrationDemo/App_Start/RouteConfig.cs
+++ AspNetAngular2IntegrationDemo/App_Start/RouteConfig.cs
@@ -12,12 +12,7 @@ namespace AspNetAngular2IntegrationDemo
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapMvcAttributeRoutes();
         }
     }
 }
---
+++ AspNetAngular2IntegrationDemo/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetAngular2IntegrationDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("{*path}")]
+        public ActionResult Index()
+        {
+            return File("~/Content/index.html", "text/html");
+        }
+    }
+}
